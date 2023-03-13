cdb225b23cf5d086d23b0c40c393ccd3837f12a1
Trqz <joaomarcostroquez@gmail.com>
2023-03-13 17:44:43 -0400

    m314904@COMPLETED Code in the default script

--- 2-Essentials 3D project/Assets/Scripts/HelloWorld.cs
+++ 2-Essentials 3D project/Assets/Scripts/HelloWorld.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string myMessage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(myMessage);
     }
 }
